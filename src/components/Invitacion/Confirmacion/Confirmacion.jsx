9e447f3bd2283f6a407f4041665964dc4d206dad
Jorge <jorge.gonzalez.fuentes.dev@gmail.com>
2026-09-25 04:15:48 +0200

    Ajuste formulario

--- src/components/Invitacion/Confirmacion/Confirmacion.jsx
+++ src/components/Invitacion/Confirmacion/Confirmacion.jsx
@@ -471,7 +471,7 @@ export default function Confirmacion() {
             <motion.div
                 className="
                     flex flex-col items-center justify-center
-                    w-90 sm:w-xl md:w-2xl lg:w-3xl xl:w-5xl 2xl:w-7xl
+                    w-88 sm:w-xl md:w-2xl lg:w-3xl xl:w-5xl 2xl:w-7xl
                     p-5 sm:p-10 mt-5
                     bg-[#F3EFE4]/80 backdrop-blur-md
                     rounded-3xl
